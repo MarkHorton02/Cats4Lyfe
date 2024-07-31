dffd76e821db17f51db8988b455950e21fddba63
JohnPaul Swaine <swainejohn301@gmail.com>
2024-07-31 14:28:47 +0100

    added about to main app.jsx and added a home.jsx for the main webpage

--- src/About.jsx
+++ src/About.jsx
@@ -1,17 +1,24 @@
-<div>
-    <h1>About Us</h1>
+const About = () => {
+    return(
+    <div>
+        <h1>About Us</h1>
 
-    <h3>Our company began when the three of us discovered we all enjoy stealing cats. We decided it would be fun to steal cats and sell them for a profit.</h3>
-    <div>
-        <img src="" />
-        <h5>Mark</h5>
+        <p>Our company began when the three of us discovered
+            we all enjoy stealing cats. We decided it would be
+            fun to steal cats and sell them for a profit.</p>
+        <div>
+            <h5>Mark</h5>
+            <img src="" />
+        </div>
+        <div>
+            <h5>John</h5>
+            <img src='./assets/JohnImageAboutUs.jpg'></img>
+        </div>
+        <div>
+            <h5>Kyle</h5>
+            <img src="" />
+        </div>
     </div>
-    <div>
-        <img src="./assets/John-image-about-us.jpg" />
-        <h5>John</h5>
-    </div>
-    <div>
-        <img src="" />
-        <h5>Kyle</h5>
-    </div>
-</div>
+)}
+
+export default About
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
+import About from './About'
+import './Home'
 
 const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      
+      <h1>Testing</h1>
+
+      <nav>
+        <link>Home</link>
+        <Link to='/About'>About</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/About' element={ <About /> }></Route>
+        <Route path='/' element={ <Home /> }></Route>
+      </Routes>
+
     </div>
   )
 }
