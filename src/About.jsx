9d8d45b43d5bfa80e1558dc5cb31e2e4f1563b32
JohnPaul Swaine <swainejohn301@gmail.com>
2024-07-31 13:30:42 +0100

    updated and added about us page to be linked via react router

---
+++ src/About.jsx
@@ -0,0 +1,17 @@
+<div>
+    <h1>About Us</h1>
+
+    <h3>Our company began when the three of us discovered we all enjoy stealing cats. We decided it would be fun to steal cats and sell them for a profit.</h3>
+    <div>
+        <img src="" />
+        <h5>Mark</h5>
+    </div>
+    <div>
+        <img src="./assets/John-image-about-us.jpg" />
+        <h5>John</h5>
+    </div>
+    <div>
+        <img src="" />
+        <h5>Kyle</h5>
+    </div>
+</div>
