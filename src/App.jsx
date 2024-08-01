718bf64cecc709ad95f8b744c206489656c89728
Mark Horton <m.a.horton02@gmail.com>
2024-08-01 14:39:42 +0100

    its less broken? yay?

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,79 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Checkout from './pages/Checkout'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [total, setTotal] = useState()
+  const [open, setOpen] = useState(false);
+  const [catsInBasket, setCatsInBasket] = useState ([])
+  const navigate = useNavigate()
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const modalRef = useRef();
+
+  const handleClickOutside = (event) => {
+    if (modalRef.current === event.target) {
+      handleClose();
+    }
+  };
+
+  const handleMove = () => {
+    setOpen(false);
+    navigate('/Checkout')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+
+      <nav className='navBar'>
+        <Link to='/'>Home</Link>
+        <Link to='/About'>About</Link>
+        <Link to='/Checkout'>Checkout</Link>
+        <button id="basket" onClick={handleOpen}>Basket</button>
+      </nav>
+
+      <Routes>
+        <Route path='/About' element={<About />}></Route>
+        <Route path='/' element={<Home setCatsInBasket = {setCatsInBasket} />}></Route>
+        <Route path='/Checkout' element={<Checkout catsInBasket = {catsInBasket} />}>Checkout</Route>
+      </Routes>
+
+      {open && (
+        <div className="modalBg" ref={modalRef} onClick={handleClickOutside}>
+          
+          <div className='iconModal'>
+
+            <div className='Products'>
+            {catsInBasket.map((cat, index) => {
+            return (
+                <div key={index}> 
+                  <h2>{cat.name}</h2>
+                  <h2>£{cat.price}</h2>
+                </div>
+            )
+        })}
+        <h2>Total Price: £{catsInBasket.reduce((total, cat) => total+cat.price, 0)}</h2>
+            </div>
+
+            <button id='x' onClick={handleClose}>X</button>
+            <button id='close' onClick={handleMove}>Go To Checkout</button>
+
+          </div>
+        </div>
+      )
+    }
+
+    </div>
   )
 }
 
